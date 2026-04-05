b539b75c61914a3bf5e1565519a643a63c2968fc
Mohit Gaywar <mohitpurbhe1998@gmail.com>
2026-04-05 18:34:34 +0530

    Added exception and services

--- src/main/java/com/noteverse/ai_notes/controller/NotesController.java
+++ src/main/java/com/noteverse/ai_notes/controller/NotesController.java
@@ -2,15 +2,17 @@ package com.noteverse.ai_notes.controller;
 
 
 import com.noteverse.ai_notes.dto.NotesRequestDTO;
+import com.noteverse.ai_notes.dto.NotesResponseDTO;
 import com.noteverse.ai_notes.entity.Notes;
 import com.noteverse.ai_notes.service.NotesService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/notes")
+@RequestMapping("/api/v1/notes")
 public class NotesController {
 
     private final NotesService notesService;
@@ -20,10 +22,7 @@ public class NotesController {
     }
 
     @PostMapping
-    public Notes saveNotes(@RequestBody NotesRequestDTO notesRequestDTO){
-        Notes notes = new Notes();
-        notes.setVideoUrl(notesRequestDTO.getVideoUrl());
-        notes.setContent(notesRequestDTO.getContent());
-        return notesService.saveNotes(notes);
+    public ResponseEntity<NotesResponseDTO> saveNotes(@RequestBody NotesRequestDTO notesRequestDTO){
+        return ResponseEntity.ok(notesService.saveNotes(notesRequestDTO));
     }
 }
--- src/main/java/com/noteverse/ai_notes/service/NotesService.java
+++ src/main/java/com/noteverse/ai_notes/service/NotesService.java
@@ -1,8 +1,10 @@
 package com.noteverse.ai_notes.service;
 
+import com.noteverse.ai_notes.dto.NotesRequestDTO;
+import com.noteverse.ai_notes.dto.NotesResponseDTO;
 import com.noteverse.ai_notes.entity.Notes;
 
 public interface NotesService {
 
-     Notes saveNotes(Notes notes);
+     NotesResponseDTO saveNotes(NotesRequestDTO notesRequestDTO);
 }
